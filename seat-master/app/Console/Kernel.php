25f57afa66f59109434c6d93c9ee777956ac3906
Rickno221 <41172709+Rickno221@users.noreply.github.com>
2018-07-13 03:51:46 +0000

    Add files via upload

---
+++ seat-master/app/Console/Kernel.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Console;
+
+use Exception;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Seat\Services\Models\Schedule as DbSchedule;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     *
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+
+        // Check that the schedules table exists. This
+        // could cause a fatal error if the app is
+        // still being setup or the db has not yet
+        // been configured. This is a relatively ugly
+        // hack as this schedule() method is core to
+        // the framework.
+        try {
+
+            DB::connection();
+            if (! Schema::hasTable('schedules'))
+                throw new Exception('Schema schedules does not exist');
+
+        } catch (Exception $e) {
+
+            return;
+        }
+        // Load the schedule from the database
+        foreach (DbSchedule::all() as $job) {
+
+            $command = $schedule->command($job['command'])
+                ->cron($job['expression']);
+
+            // Check if overlapping is allowed
+            if (! $job['allow_overlap'])
+                $command = $command->withoutOverlapping();
+
+            // Check if maintenance mode is allowed
+            if ($job['allow_maintenance'])
+                $command = $command->evenInMaintenanceMode();
+
+            if ($job['ping_before'])
+                $command = $command->pingBefore($job['ping_before']);
+
+            if ($job['ping_after'])
+                $command->thenPing($job['ping_after']);
+
+        }
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
---
+++ seat-master/config/services.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'ses' => [
+        'key'    => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => 'us-east-1',
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe'    => [
+        'model'  => App\User::class,
+        'key'    => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+    // If you want to make use of SSO via EVEOnline, create
+    // an application at https://developers.eveonline.com/applications
+    // and fill in the ClientID and ClientSecret in the .env file
+    'eveonline' => [
+        'client_id'     => env('EVE_CLIENT_ID'),
+        'client_secret' => env('EVE_CLIENT_SECRET'),
+        'redirect'      => env('EVE_CALLBACK_URL'),
+    ],
+
+];
